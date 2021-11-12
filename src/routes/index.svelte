0cbd9d35e0625a156f5ea0e4e6b105c310c638c4
Brian <contact@noimbrian.com>
2021-11-12 09:16:11 -0800

    feat: view layer

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+	export const load = async () => {
+		return {
+			props: {
+				quote: 'This is my quote'
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let quote: string;
+</script>
+
+<main>
+	<h1>{quote}</h1>
+</main>
+
+<style lang="postcss">
+    main {
+        @apply w-screen h-screen bg-purple-500 text-white flex justify-center items-center;
+    }
+	h1 {
+        @apply text-4xl;
+        font-family: 'Dancing Script', cursive;
+	}
+</style>
