f9c002d22ff1011eec7047452683ba3fb5659fd3
Brian <contact@noimbrian.com>
2021-11-12 09:28:56 -0800

    feat: SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,16 +12,29 @@
 	export let quote: string;
 </script>
 
+<svelte:head>
+    <meta property="og:title" content="StartupQuotes.io" />
+    <meta property="og:description" content={quote} />
+</svelte:head>
+
 <main>
 	<h1>{quote}</h1>
+    <footer>
+        Startup Quotes
+    </footer>
 </main>
 
 <style lang="postcss">
     main {
-        @apply w-screen h-screen bg-purple-500 text-white flex justify-center items-center;
+        @apply w-screen h-screen bg-purple-900 text-white flex justify-center items-center relative;
     }
 	h1 {
         @apply text-4xl;
         font-family: 'Dancing Script', cursive;
 	}
+    footer {
+        @apply absolute bottom-10 left-1/2 -translate-x-1/2 text-sm;
+        letter-spacing: 0.2ch;
+        font-family: monospace;
+    }
 </style>
